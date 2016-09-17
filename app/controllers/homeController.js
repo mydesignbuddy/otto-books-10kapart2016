3cc152e8eb28f6c7ce50c2e74d2341c07ba24ee7
Buddy Toups <buddyt@blinds.com>
2016-09-16 19:18:38 -0500

    more updates with responsive updates

--- app/controllers/homeController.js
+++ app/controllers/homeController.js
@@ -17,8 +17,3 @@ router.get('/bookcover/:name', function (req, res, next) {
   res.sendFile(path.join(__dirname, '../../public/img/loading.svg'));
 });
 
-router.get('/main.js', function(req, res)
-{
-    res.setHeader('Content-Type', 'application/javascript');
-    res.send('window.onBeforeUnload=function(e){return e=e||window.event,console.log("delaying page exit"),setTimeout(function(){console.log("do something within the delay")},5e3),!0},!function(e){var t=function(e,t){if(document.querySelectorAll)t=document.querySelectorAll(e);else{var n=document,o=n.styleSheets[0]||n.createStyleSheet();o.addRule(e,"f:b");for(var l=n.all,r=0,u=[],i=l.length;i>r;r++)l[r].currentStyle.f&&u.push(l[r]);o.removeRule(0),t=u}return t},n=new Array,o=t("img.lazy");setInterval(function(){o=t("img.lazy"),n=[];for(var e=0;e<o.length;e++)n.push(o[e])},1e3)}(this);');
-});
