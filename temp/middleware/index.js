a577a2632ffec4b3bcc0c38852a366fde3b03606
Buddy Toups <buddyt@blinds.com>
2016-09-27 13:07:32 -0500

    remove first try code

--- temp/middleware/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var utils = require('../lib/utils');
-exports = module.exports = utils.requireDir(__dirname);
-
-exports.logger = require('morgan');
-exports.slash  = require('express-slash');
